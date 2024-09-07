f52f9538ec93eda60f61dda987a4ab47ec660fa1
Marcio Travain <travain@gmail.com>
2024-09-07 17:59:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+// let dados = [
+//"Rebeca Andrade", 
+//"Caio Bonfim",
+//"William Lima"]
+;// Variável contendo lista de texto (array) que usa colchetes, foi usada no ex inicial, depois trocamos os nomes pelos objetos criados na sequencia
+
+let dados = [
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, com 6 medalhas. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um ícone do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica medalha ouro"
+       },
+    
+    {
+      titulo: "Caio Bonfim",
+      descricao: "Caio Bonfim é um atleta brasileiro de marcha atlética, conhecido por suas conquistas em diversas competições internacionais. Sua dedicação e disciplina o levaram a alcançar o pódio em importantes eventos esportivos.",
+      link: "https://pt.wikipedia.org/wiki/Caio_Bonfim",
+      tags: "podio pódio marxa atletica"
+    },
+    
+    {
+      titulo: "William Lima",
+      descricao: "William Lima é um atleta brasileiro de judô, com diversas medalhas em competições nacionais e internacionais. Sua habilidade e técnica o destacam no cenário do judô brasileiro.",
+      link: "https://pt.wikipedia.org/wiki/Willian_Lima",
+      tags: "judo judoca luta"
+    }
+];
+  
